4bfd179318158f4bbe13d1026036f9d741617351
XG <homdon>
2025-08-05 12:53:28 -0500

    test 3

---
+++ jenkins-vm/gemini-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Run the CLI: Execute the following command in your terminal:
+#npx https://github.com/google-gemini/gemini-cli
+
+#Or install it with:
+
+npm install -g @google/gemini-cli
+
+#Then, run the CLI from anywhere:
+
+gemini
+
+echo "---------------------------"
+echo "gemini: $(gemini)"
+echo "---------------------------"
+echo
+echo
+echo " INSTALLATION GEMINI COMPLETE "
